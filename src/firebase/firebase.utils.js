d32012f62c6c71d8765e8756ec4c93f1b34025c6
Pablo Freire <pablofreire6@gmail.com>
2021-06-25 09:28:58 -0300

    remove log

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,8 +2,6 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
-console.log(process.env);
-
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
