021a1151dfa566b376279c43e8915d8aeb099f9a
Pablo Freire <pablofreire6@gmail.com>
2021-06-25 09:28:28 -0300

    remove firebase env variables

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,13 +2,15 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
+console.log(process.env);
+
 const config = {
-  apiKey: "AIzaSyBX9MhgyVerXIk54bVtQeuOnjp3cUv8YUo",
-  authDomain: "crwn-db-52dd0.firebaseapp.com",
-  projectId: "crwn-db-52dd0",
-  storageBucket: "crwn-db-52dd0.appspot.com",
-  messagingSenderId: "1098192968225",
-  appId: "1:1098192968225:web:ee521c0fdc1785a5a3e160"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
